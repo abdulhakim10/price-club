5efa6421ba287865e0384da499e14ebd174d7820
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-10-07 00:39:54 +0600

    create chart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Pricing from './components/Pricing/Pricing';
+import AssignmentMarks from './components/AssignmentMarks/AssignmentMarks';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
      <h1 className='text-5xl font-bold underline'>This is a big big header..!</h1>
      <p>This is a simple paragraph.</p>
      <Pricing></Pricing>
+     <AssignmentMarks></AssignmentMarks>
     </div>
   );
 }
